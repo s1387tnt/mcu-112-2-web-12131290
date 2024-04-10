8ff722f0540b94cd3b4cbbec8954f44f780d5af4
s1387tnt <s1387tnt@gmail.com>
2024-04-10 20:17:40 +0800

    feat(ts): 利用陳列紀錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10)
-);
-
-console.log(product);
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+];
+console.log(products);
