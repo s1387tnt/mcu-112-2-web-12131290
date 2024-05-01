e277b41de4054f54c33e913c56f7ee6e577b6438
s1387tnt <s1387tnt@gmail.com>
2024-05-01 21:22:31 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
