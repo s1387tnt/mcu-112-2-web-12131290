798caa69f9c14f902c9fd793e2f92c461bdfbb36
s1387tnt <s1387tnt@gmail.com>
2024-05-01 20:07:58 +0800

    feat(header): 新增標題元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardListComponent],
+  imports: [HeaderComponent, ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
